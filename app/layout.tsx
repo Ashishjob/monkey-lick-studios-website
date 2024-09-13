7b9fae6af26ec866185b8b692224352f58507fa3
Ashishjob <ashishjob104@gmail.com>
2024-09-13 00:51:56 -0500

    refactor: add url to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Monkey Lick Studios",
   description: "Monkey Lick Studios is a game development studio that creates fun and engaging games.",
+  metadataBase: new URL("https://www.monkeylick.studio"),
   openGraph: {
     images: [
       {
